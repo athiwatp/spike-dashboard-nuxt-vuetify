71a611d1605586cb02a7cc753e95989ca646f6a8
ltodeschini <Ramones$1>
2018-06-17 01:00:08 +0200

    loading data from firebase

--- middleware/erogated_posts.js
+++ middleware/erogated_posts.js
@@ -1,6 +1,19 @@
-// import axios from 'axios'
+import axios from 'axios'
 
 export default function ({ store, route }) {
   // axios.get('http://my-stats-api.com')
   console.log('running middleware')
+  axios.get('https://xxxxxxx.firebaseio.com/erogated_posts.json')
+    .then(function (response) {
+      var result = []
+      for (var p in response.data) {
+        if (response.data.hasOwnProperty(p)) {
+          result.push(response.data[p])
+        }
+      }
+      store.commit('erogated_posts/setCards', result)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 }
--- store/erogated_posts.js
+++ store/erogated_posts.js
@@ -4,13 +4,17 @@ export const state = () => ({
 })
 
 export const getters = {
-  getCards (state) { return getErogatedPosts() }
+  getCards (state) { return state.cards }
 }
 
-function getErogatedPosts () {
-  return [
-    { title: 'Pre-fab homes', src: '/static/doc-images/cards/house.jpg', flex: 6 },
-    { title: 'Favorite road trips', src: '/static/doc-images/cards/road.jpg', flex: 6 },
-    { title: 'Best airlines', src: '/static/doc-images/cards/plane.jpg', flex: 6 }
-  ]
+export const mutations = {
+  setCards (state, posts) {
+    console.log(posts)
+    var cards = []
+    posts.forEach(element => {
+      cards.push({ title: element.title, src: element.link, flex: 6 })
+    })
+
+    state.cards = cards
+  }
 }
